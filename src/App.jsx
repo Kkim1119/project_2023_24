d6cb233d8d6cc0220277d9d8101b7b35a237b09e
Kkim1119 <104954648+Kkim1119@users.noreply.github.com>
2023-10-14 16:38:24 -0700

    created button and label, will add more stuff later

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+function MyButton() {   //Creating a button
   return (
-    <button> I'm a button</button>
+    <button>
+      I'm a button
+    </button>
   );
 }
 
-export default App
+export default function MyApp() {
+return(
+  <div>
+    <hi>Welcome to my app</hi>
+    <MyButton />
+  </div>
+);
+
+}
